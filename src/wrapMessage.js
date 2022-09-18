7cf853abef49e6fc34bd41233c17d2994237b712
hungran <26101787+hungran@users.noreply.github.com>
2022-09-18 21:44:47 +0700

    change to markdown

--- src/wrapMessage.js
+++ src/wrapMessage.js
@@ -4,7 +4,7 @@ module.exports = async function (message, parse_mode) {
     if(!message) {
         throw new Error('no message found. Exiting')
     }
-    if(parse_mode != "MarkdownV2") {
+    if(parse_mode != "MarkdownV2" || parse_mode != "Markdown") {
         return message
     }
     const r = await message.toString()
